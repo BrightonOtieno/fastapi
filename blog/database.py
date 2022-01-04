c6d5450693f5bef415e5ee6c6f4d9fe3f06554c5
brighton hoods <brightonhoods@gmail.com>
2022-01-04 12:01:20 +0300

    Declare Mapping

--- blog/database.py
+++ blog/database.py
@@ -1,5 +1,8 @@
 from sqlalchemy import create_engine, engine
+from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args ={"check_same_thread":False})
+
+Base = declarative_base()
