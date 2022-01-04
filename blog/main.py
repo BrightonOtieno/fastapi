4af74dbf23bbaed3a25885a9f8df078269a2d96d
brighton hoods <brightonhoods@gmail.com>
2022-01-04 19:50:55 +0300

    save client data to db

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from . import schemas, models
-from .database import engine
+from .database import SessionLocal, engine
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
-# create a post
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# create post
 @app.post('/blog')
-def create_post(request: schemas.Blog):
-    return request
+def create_post(request: schemas.Blog, db: Session = Depends(get_db)):
+    # add the post data into db as a new blog post
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    # refresh it so the we can easily return it as feedback
+    return new_blog
