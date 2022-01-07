124a6eab4c87e735c581d8bf99a6df3a78813bfb
brighton hoods <brightonhoods@gmail.com>
2022-01-07 09:40:37 +0300

    refactor user routers with user repository

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, status, Response, HTTPException
 from .. import schemas, database, models
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
+
+from ..repository import user
 
 get_db = database.get_db
 
@@ -9,27 +10,15 @@ router = APIRouter(
     prefix='/user',
     tags=['users']
 )
-# password hashing context
-pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 # CREATE USER
 @router.post('/', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    hashedPassword = pwd_cxt.hash(request.password)
-    new_user = models.User(
-        name=request.name, email=request.email, password=hashedPassword)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    return user.create_user(request, db)
 
 
 # GET User
 @router.get('/{id}', response_model=schemas.ShowUser)
 def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} does not exist")
-    return user
+    return user.get_user(id, db)
